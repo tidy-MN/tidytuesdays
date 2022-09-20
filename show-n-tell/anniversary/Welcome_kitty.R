6f91589f8ac35d1cea319d1c75c6f55ee19f4584
air <dKvale@users.noreply.github.com>
2022-09-20 09:41:16 -0500

    Update Welcome_kitty.R

--- show-n-tell/anniversary/Welcome_kitty.R
+++ show-n-tell/anniversary/Welcome_kitty.R
@@ -1,6 +1,6 @@
 .First <- function() {
 
-  message("Good morning Kristie!.")
+  message("Good morning Kristie!")
   
   get_cat <- function(width = 400, height = 400){
     r <- httr::GET(paste("http://theoldreader.com/kittens", width, height, sep = "/"))
@@ -9,7 +9,7 @@
   }
   
   # Plot the cat for the day
-  graphics::plot(data.frame(x = 1:2, y = 1:2), axes = F, main = "Greetings Kristie!", xlab = "", ylab = "", col = "white")
+  graphics::plot(data.frame(x = 1:2, y = 1:2), axes = F, main = "I am cat!", xlab = "", ylab = "", col = "white")
   
   graphics::rasterImage(get_cat(), 1, 1, 2, 2)
 }
